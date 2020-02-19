e87920241e95b2347db96596a3ddec4c0850eb87
Martin Beletskyi-Moawad <martbelmoaw@gmail.com>
2020-02-19 19:14:32 +0300

    final solution for cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,24 +1,30 @@
 class VigenereCipheringMachine {
+  constructor(direct = true) {
+    this.direct = direct;
+  }
+
   encrypt(input, key) {
     if (!input || !key) throw new Error();
 
     const formatted_input = input.toUpperCase().split("");
     const formatted_key = key.toUpperCase().split("");
 
-    return formatted_input
-      .map(letter => {
-        const letter_code = letter.charCodeAt(0) - 65;
-        let result = letter;
+    const result = formatted_input.map(letter => {
+      const letter_code = letter.charCodeAt(0) - 65;
+      let result = letter;
 
-        if (letter_code >= 0 && letter_code <= 25) {
-          result = String.fromCharCode(
-            (letter_code + formatted_key[0].charCodeAt(0) - 65) % 26 + 65
-          );
-          formatted_key.push(formatted_key.shift());
-        }
-        return result;
-      })
-      .join("");
+      if (letter_code >= 0 && letter_code <= 25) {
+        result = String.fromCharCode(
+          ((letter_code + formatted_key[0].charCodeAt(0) - 65) % 26) + 65
+        );
+        formatted_key.push(formatted_key.shift());
+      }
+      return result;
+    });
+
+    !this.direct && result.reverse();
+
+    return result.join("");
   }
 
   decrypt(input, key) {
@@ -27,20 +33,22 @@ class VigenereCipheringMachine {
     const formatted_input = input.split("");
     const formatted_key = key.toUpperCase().split("");
 
-    return formatted_input
-      .map(letter => {
-        const letter_code = letter.charCodeAt(0) - 65;
-        let result = letter;
+    const result = formatted_input.map(letter => {
+      const letter_code = letter.charCodeAt(0) - 65;
+      let result = letter;
 
-        if (letter_code >= 0 && letter_code <= 25) {
-          result = String.fromCharCode(
-            ((letter_code - (formatted_key[0].charCodeAt(0) - 65) + 26) % 26) + 65
-          );
-          formatted_key.push(formatted_key.shift());
-        }
-        return result;
-      })
-      .join("");
+      if (letter_code >= 0 && letter_code <= 25) {
+        result = String.fromCharCode(
+          ((letter_code - (formatted_key[0].charCodeAt(0) - 65) + 26) % 26) + 65
+        );
+        formatted_key.push(formatted_key.shift());
+      }
+      return result;
+    });
+
+    !this.direct && result.reverse();
+
+    return result.join("");
   }
 }
 
